4db4850ce31db4a0690b86652a7625f12a120a3a
kraftwerk28 <kefirchik3@gmail.com>
2021-02-06 13:44:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# gtranslate.nvim
+
+Plugin for neovim to translate text through https://translate.googleapis.com
+written in Lua.
+
+Requires Neovim v0.5
+
+### Installation
+The best way is to use [packer.nvim](https://github.com/wbthomason/packer.nvim),
+because it allows installing libs from LuaRocks out of the box.
+The plugin requires `lua-cjson` and `http` libraries to work:
+
+```lua
+local function load(use)
+  use {'kraftwerk28/gtranslate.nvim', rocks = {'lua-cjson', 'http'}}
+  -- ...other plugins
+end
+
+vim.cmd 'packadd packer.nvim'
+local packer = require 'packer'.startup{load}
+```
+
+Though you can install rocks manually and then edit `package.path` to allow
+nvim to require them
+
+### Usage
+
+```vimscript
+:Translate <from-language> <to-language>
+:Translate <to-language> " In this case, <from-language> is automatic
+```
+
+Select the text to translate (Visual mode), then type `:Translate <language>`
---
+++ lua/gtranslate.lua
@@ -0,0 +1,55 @@
+local M = {}
+
+local function require_libs()
+  local ok, cjson = pcall(require, 'cjson')
+  if not ok then
+    print('Module `lua-cjson` not installed.')
+    return false
+  end
+  local ok, request = pcall(require, 'http.request')
+  if not ok then print('Module `http` not installed.') end
+  local _, utils = pcall(require, 'http.util')
+
+  return true, cjson, request, utils
+end
+
+function M.translate(text, ...)
+  local ok, cjson, request, utils = require_libs()
+  if not ok then return end
+
+  local args = {...}
+  local from_lng, to_lng
+  if #args == 1 then
+    from_lng, to_lng = 'auto', args[1]
+  elseif #args == 2 then
+    from_lng, to_lng = args[1], args[2]
+  else
+    return text
+  end
+
+  local base = 'https://translate.googleapis.com'
+  local path = '/translate_a/single'
+  local params = {
+    client = 'gtx',
+    sl = from_lng,
+    tl = to_lng,
+    dt = 't',
+    q = text,
+  }
+  local url = string.format('%s%s?%s', base, path, utils.dict_to_query(params))
+
+  local req = request.new_from_uri(url)
+  local _, stream = req:go(5)
+  local ok, result = pcall(function()
+    local b = cjson.decode(stream:get_body_as_string())
+    return b[1][1][1]
+  end)
+  if ok then
+    return result
+  else
+    print('Failed to translate')
+    return text
+  end
+end
+
+return M
